3930b7771ec93bed8f0bdcb1345c796914f47cb4
muharrem gem <muharremgem28@gmail.com>
2022-10-08 12:31:18 +0300

    commit

--- rc02_jsx_comp_basicStyling/src/App.js
+++ rc02_jsx_comp_basicStyling/src/App.js
@@ -1,7 +1,23 @@
+//! Componentler, HTML,CSS ve JS kodlarindan meydana gelmiş
+//! bir kod parcacigidir.
+//! Bir componentin return () kismi ise JSX kodlari icerir.
+//! JSX'in kendine gore bazi kurallari vardir. Ornegin bir
+//! component eger birden fazla html elementi dondurmesi
+//! gerekirse bunlari tek bir container elemani ile sarmayalarak
+//! dondurmelidir.container icin div, article, header, <> v.b
+//! herhangi bir element kullanilabilir.
+
 import React from "react";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
 
 function App() {
-  return <h1>JSX & COMPONENTS & BASİC STYLING</h1>;
+  return (
+    <div>
+      <Header />
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
---
+++ rc02_jsx_comp_basicStyling/src/components/Header.jsx
@@ -0,0 +1,16 @@
+// Harici bir dosta icerisinde olusturulan bir component export default ile proje icerisinde herhangi bir yerde kullanima acilmis olur. Kullanmak icin hedef dosyada import ... from "./..." ile import yapmak yeterlidir.
+
+import React from 'react'
+
+class Header extends React.Component {
+    render(){
+        return(
+            <div>
+                muharrem
+            </div>
+
+        )
+    }
+}
+
+export default Header;
