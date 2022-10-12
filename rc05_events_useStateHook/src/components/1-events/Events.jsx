bcfeb6115051958368004c620a7f94236ef4adfc
muharrem gem <muharremgem28@gmail.com>
2022-10-12 20:39:09 +0300

    session5

--- rc05_events_useStateHook/src/components/1-events/Events.jsx
+++ rc05_events_useStateHook/src/components/1-events/Events.jsx
@@ -4,11 +4,27 @@ export const Events = () => {
   const handleClick = () => {
     alert("Btn Clicked");
   };
+  const handleClear = (msg) => {
+    alert(msg);
+  };
+
+  const handleChange = (event) => {
+    console.log(event.target);
+  };
 
   return (
     <div className="container text-center mt-4">
       <button onClick={handleClick} className="btn btn-success">
         Click
+      </button>
+      <button
+        onClick={() => handleClear("Clear Btn Clicked")}
+        className="btn btn-danger"
+      >
+        Clear
+      </button>
+      <button onClick={handleChange} className="btn btn-dark">
+        Change
       </button>
     </div>
   );
