fd1c7940a5fbb00e2b099d50a0dc25096f685be6
muharrem gem <muharremgem28@gmail.com>
2022-10-12 22:39:27 +0300

    useState counter

--- rc05_events_useStateHook/src/components/3-useStateHook/UseStateCounter.jsx
+++ rc05_events_useStateHook/src/components/3-useStateHook/UseStateCounter.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from "react";
+import { useState } from "react";
 
 const UseStateCounter = () => {
-  return (
-    <div>UseStateCounter</div>
-  )
-}
 
-export default UseStateCounter
+const [count, setCount] = useState(0);
+const inc = () => {
+    setCount(count + 1);
+};
+
+  return (
+    <div className="container text-center mt-4">
+      <h1>USE STATE HOOK</h1>
+      <h2 className="display-4 text-danger">COUNT:{count}</h2>
+      <button onClick={inc} className="btn btn-success">INC</button>
+      <button onClick={()=>setCount(0)} className="btn btn-dark">CLR</button>
+      <button onClick={()=> setCount(count - 1)} className="btn btn-danger">DEC</button>
+    </div>
+  );
+};
+
+export default UseStateCounter;
